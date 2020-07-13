e0d9103d6a5cd0ec89da8b4956c1c0a2d43b4589
SJH <shenjunhong@meizu.com>
2020-07-13 18:44:39 +0800

    提交代码

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
 
-import { Router, Route, Link } from 'react-router'
+import { Link } from 'react-router'
 class Edit extends Component {
 
   componentDidMount(){
-    console.log(this.props.match.params);
-
-    console.log(this.props.history.location.state);
-
     this.props.history.replace('/detail');
 
     this.props.history.goBack();
   }
 
-  const User = React.createClass({
-    componentDidMount() {
-      this.setState({
-        // 路由应该通过有用的信息来呈现，例如 URL 的参数
-        user: findUserById(this.props.params.userId)
-      })
-    },
-  
-    render() {
-      return (
-        <div>
-          <h2>{this.state.user.name}</h2>
-          {/* 等等。 */}
-        </div>
-      )
-    }
-  })
+
 
   render() {
     return (
@@ -37,9 +17,7 @@ class Edit extends Component {
         edit
         <ul>
             {/* 在应用中用 Link 去链接路由 */}
-            {this.state.users.map(user => (
-              <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
-            ))}
+   
           </ul>
       </div>
     );
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Home = React.lazy(()=>import(/*webpackchunkname:Home*/'./pages/Home' ))
 const Edit = React.lazy(()=>import(/*webpackchunkname:Edit*/'./pages/Edit' ))
+const Drag = React.lazy(()=>import(/*webpackchunkname:Drage*/'./pages/Drag'))
+const Sort = React.lazy(()=>import(/*webpackchunkname:Drage*/'./pages/Sort'))
 
 export default [
     {
@@ -12,5 +14,13 @@ export default [
     {
         path:'/edit/:id',
         component:Edit
+    },
+    {
+      path:'/drag/index',
+      component:Drag
+    },
+    {
+      path:'/sort/index',
+      component:Sort
     }
 ]
